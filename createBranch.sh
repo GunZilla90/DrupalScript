4a53e2658b9861873317983505fad12a4f10ead8
Kevin <ssj_kevin@hotmail.com>
2021-01-04 18:04:36 +0100

    changes made on

--- createBranch.sh
+++ createBranch.sh
@@ -5,8 +5,9 @@ PROTECTEDBRANCH="master"
 CURRENTBRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 if [ "$PROTECTEDBRANCH" == "$CURRENTBRANCH" ]; then
-    RANDOMBRANCH=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
-
+    #RANDOMBRANCH=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+    DATE = $(date)
+    RANDOMBRANCH = "update/drupal_update_$DATE"
     git checkout -b $RANDOMBRANCH master
 
 
@@ -16,5 +17,8 @@ if [ "$PROTECTEDBRANCH" == "$CURRENTBRANCH" ]; then
     DATE = $(date)
 
     git commit -m "changes made on $DATE"
+
+    git push --set-upstream origin $RANDOMBRANCH
+
 fi
 
