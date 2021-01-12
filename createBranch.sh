23ef7408ae2c8b44e19465a26dc75af71dd79e82
Kevin <ssj_kevin@hotmail.com>
2021-01-12 09:33:58 +0100

    changes made on

--- createBranch.sh
+++ createBranch.sh
@@ -5,9 +5,9 @@ PROTECTEDBRANCH="master"
 CURRENTBRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 if [ "$PROTECTEDBRANCH" == "$CURRENTBRANCH" ]; then
-    #RANDOMBRANCH=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+    RANDOMBRANCH=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
     DATE = $(date)
-    RANDOMBRANCH = $(cat "update/drupal_update_test")
+    #RANDOMBRANCH = $(cat "update/drupal_update_test")
     git checkout -b $RANDOMBRANCH
 
 
